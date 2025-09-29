8659cd96e2faf11f228437fca654646baf5abd2e
edupoly <info.edupoly@gmail.com>
2025-09-29 12:59:04 +0530

    pug-template-engine

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ var app = express();
 
 app.set("view engine", "pug");
 
-var products = JSON.parse(fs.readFileSync("products.txt").toString());
+var allproducts = JSON.parse(fs.readFileSync("products.txt").toString());
 app.get("/products", function (req, res) {
   // res.send(products);
-  res.render("productslist", { hobby: "Cricket", marks: [10, 20, 15] });
+  res.render("productslist", {
+    hobby: "Cricket",
+    marks: [10, 20, 15],
+    products: allproducts.products,
+  });
+});
+app.get("/details/:id", function (req, res) {
+  console.log(req.params.id);
+  var selectedProduct = allproducts.products.find((pr) => {
+    if (pr.id == req.params.id) {
+      return true;
+    }
+  });
+  console.log(selectedProduct);
+  res.render("productDetails", { product: selectedProduct });
 });
 app.listen(3500, () => {
   console.log("Server running on 3500");
