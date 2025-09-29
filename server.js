34ed72dd44168cb0b660688b42dda06e921e42d0
edupoly <info.edupoly@gmail.com>
2025-09-29 11:49:02 +0530

    template-engine-pug

--- server.js
+++ server.js
@@ -1,62 +1,18 @@
 var express = require("express");
 var cors = require("cors");
-var bodyParser = require("body-parser");
 var fs = require("fs");
 var app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.post("/addStudent", (req, res) => {
-  var students = fs.readFileSync("students.txt").toString();
-  console.log(typeof students);
-  console.log(typeof JSON.parse(students));
-  // console.log(req.body);
-  var k = JSON.parse(students);
-  // console.log(typeof k);
-  k.push(req.body);
-  console.log(k);
-  fs.writeFileSync("students.txt", JSON.stringify(k));
-  res.send("Please wait");
-});
+app.set("view engine", "pug");
 
-app.get("/add", function (req, res) {
-  console.log(req.query);
-  res.send(+req.query.num1 + +req.query.num2);
+var products = JSON.parse(fs.readFileSync("products.txt").toString());
+app.get("/products", function (req, res) {
+  // res.send(products);
+  res.render("productslist", { hobby: "Cricket", marks: [10, 20, 15] });
 });
-app.post("/add", function (req, res) {
-  console.log(req.body);
-  res.send("Undara nayana");
-});
-app.get("/abc", function (req, res) {
-  console.log("get request for abc route recieved");
-  res.header("Content-Type", "text/plain");
-  // res.contentType = "text/html";
-  res.send("AAgura babu");
-});
-
-app.post("/abc", function (req, res) {
-  console.log("POST request for abc route recieved");
-  res.send("AAgura babu");
-});
-app.put("/abc", function (req, res) {
-  console.log("PUT request for abc route recieved");
-  res.send("AAgura babu");
-});
-app.delete("/abc", function (req, res) {
-  console.log("DELETE request for abc route recieved");
-  res.send("AAgura babu");
-});
-
-app.get("/add/:n1/:n2", (req, res) => {
-  res.send(+req.params.n1 + +req.params.n2);
-});
-
-app.get("/mul/:n1/:n2", (req, res) => {
-  res.send(req.params.n1 * req.params.n2);
-});
-
 app.listen(3500, () => {
   console.log("Server running on 3500");
 });
-// http methods : GET POST PUT DELETE (CRUD)
+//install pug
+//create views folder
+//congifure views folder with app.set("view engine","pug")
